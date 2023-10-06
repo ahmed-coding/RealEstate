bcfe51e315c67e5d160546857aefc7af6bf52bd6
Ahmed M Hamza <71948774+ahmed-coding@users.noreply.github.com>
2023-10-06 21:37:32 +0300

    Complete intro, Login activities

--- app/src/main/java/com/example/realestate/activites/introActivity.java
+++ app/src/main/java/com/example/realestate/activites/introActivity.java
@@ -2,41 +2,15 @@ package com.example.realestate.activites;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.View;
-import android.widget.Button;
 
 import com.example.realestate.R;
 
 public class introActivity extends AppCompatActivity {
-private Button startBtn;
-    private static final int SPLASH_TIME_OUT = 3000; // 3 seconds
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(introActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish(); // close the splash activity
-            }
-        }, SPLASH_TIME_OUT);
-    }
-//        initView();
-
-
-    private void initView(){
-        startBtn=findViewById(R.id.startBtn);
-        startBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-    startActivity(new Intent(introActivity.this,MainActivity.class));
-            }
-        });
     }
 }
---
+++ app/src/main/java/com/example/realestate/activites/splashActivity.java
@@ -0,0 +1,42 @@
+package com.example.realestate.activites;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.realestate.R;
+
+public class splashActivity extends AppCompatActivity {
+private Button startBtn;
+    private static final int SPLASH_TIME_OUT = 3000; // 3 seconds
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(splashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish(); // close the splash activity
+            }
+        }, SPLASH_TIME_OUT);
+    }
+//        initView();
+
+
+    private void initView(){
+        startBtn=findViewById(R.id.startBtn);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+    startActivity(new Intent(splashActivity.this,MainActivity.class));
+            }
+        });
+    }
+}
