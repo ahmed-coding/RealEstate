850a248caa2439a4e751b2faf5fbfe6de6521371
Ahmed M Hamza <71948774+ahmed-coding@users.noreply.github.com>
2023-10-08 00:46:30 +0300

    fix some bugs in sign-up

--- app/src/main/java/com/example/realestate/activites/loginActivity.java
+++ app/src/main/java/com/example/realestate/activites/loginActivity.java
@@ -16,10 +16,11 @@ public class loginActivity extends AppCompatActivity {
 
     private EditText editTextEmail;
     private EditText editTextPassword;
+    SharedPreferencesHelper prefsHelper ;
 
-    private final String UserEmail = "a@gmail.com";
-    private final String UserPassword = "12345";
-    private final String fullName= "Ahmed-M-Hamzah";
+//    private final String UserEmail = "a@gmail.com";
+//    private final String UserPassword = "12345";
+//    private final String fullName= "Ahmed-M-Hamzah";
 
     private ImageView btnLogin;
 
@@ -30,27 +31,24 @@ public class loginActivity extends AppCompatActivity {
 
         editTextEmail = findViewById(R.id.edit_text_email);
         editTextPassword = findViewById(R.id.edit_text_password);
-        btnLogin = findViewById(R.id.button_login);
+        btnLogin = findViewById(R.id.button_sginup);
+        prefsHelper = new SharedPreferencesHelper(this);
+
+        String UserEmail = prefsHelper.getEmail();
+        String UserPassword = prefsHelper.getPassword();
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Get the entered email and password
                 String email = editTextEmail.getText().toString();
                 String password = editTextPassword.getText().toString();
 
                 if (email.equals(UserEmail) && password.equals(UserPassword)) {
-                    // Email and password are correct, save them in SharedPreferences
-                    SharedPreferencesHelper prefsHelper = new SharedPreferencesHelper(loginActivity.this);
-                    prefsHelper.saveUserData(fullName,email, password);
-
-                    // After saving, you can navigate to the next screen (e.g., HomeActivity)
                     Intent intent = new Intent(loginActivity.this, MainActivity.class);
                     startActivity(intent);
-                    finish(); // Finish the login activity
+                    finish();
                 } else {
-                    // Invalid login, display an error message or handle as needed
-                    // For example, show a toast message
+
                     Toast.makeText(loginActivity.this, "Invalid email or password", Toast.LENGTH_SHORT).show();
                 }
             }
--- app/src/main/java/com/example/realestate/activites/signupActivity.java
+++ app/src/main/java/com/example/realestate/activites/signupActivity.java
@@ -2,17 +2,50 @@ package com.example.realestate.activites;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.example.realestate.Models.SharedPreferencesHelper;
 import com.example.realestate.R;
 
 public class signupActivity extends AppCompatActivity {
-
+    EditText txt_email,txt_password,txt_password1, txt_fullName;
+    ImageView btnSigunup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        txt_email = (EditText) findViewById(R.id.edit_text_email);
+        txt_password = (EditText) findViewById(R.id.edit_text_password);
+        txt_password1 =  (EditText) findViewById(R.id.edit_text_password2);
+        txt_fullName =  (EditText) findViewById(R.id.edit_text_fullName);
+        btnSigunup = (ImageView) findViewById(R.id.button_sginup);
 
+        btnSigunup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent;
+                String email = txt_email.getText().toString();
+                String password = txt_password.getText().toString();
+                String password1 = txt_password1.getText().toString();
+                String fullName = txt_fullName.getText().toString();
+                if (password.equals(password1) && password1.equals(password)) {
+                    SharedPreferencesHelper prefsHelper = new SharedPreferencesHelper(signupActivity.this);
+                    prefsHelper.saveUserData(fullName, email, password);
+                    Toast.makeText(signupActivity.this , "تم انشاء الحساب بنجاح الان يمكنك تسجيل الدخول.", Toast.LENGTH_LONG).show();
+                    intent = new Intent(signupActivity.this, MainActivity.class);
 
+                    startActivity(intent);
+                    Toast.makeText(signupActivity.this, "تم انشاء الحساب بنجاح الان يمكنك تسجيل الدخول.", Toast.LENGTH_LONG).show();
+
+                    finish();
+                } else
+                    Toast.makeText(signupActivity.this, "password no match", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
